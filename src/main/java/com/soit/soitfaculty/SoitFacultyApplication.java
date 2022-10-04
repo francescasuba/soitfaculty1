6444f497829a4918b5ed61f0a54b568ccb8f8abc
Administrator <Administrator@DESKTOP-099UQRG>
2022-10-04 14:50:28 -0400

    Updated gitignore

--- src/main/java/com/soit/soitfaculty/SoitFacultyApplication.java
+++ src/main/java/com/soit/soitfaculty/SoitFacultyApplication.java
@@ -1,5 +1,12 @@
 package com.soit.soitfaculty;
 
-public class SoitFacultyApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class SoitFacultyApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SoitFacultyApplication.class, args);
+	}
 }
